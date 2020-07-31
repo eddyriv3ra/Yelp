1d74dc5907af9b366eb095cf7afbce3b972668a0
Eddy <>
2020-07-30 21:41:39 -0300

    feat: set react native debugger

--- src/components/ResultsList/ResultsList.tsx
+++ src/components/ResultsList/ResultsList.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-const ResultsList = () => {
+type ResultsListProps = {
+  title: string;
+};
+
+const ResultsList = ({ title }: ResultsListProps) => {
   return (
     <View>
-      <Text>Results List</Text>
+      <Text style={styles.title}>{title}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
 
 export default ResultsList;
--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import ResultsList from "../components/ResultsList";
 import useSearch from "../hooks/useSearch";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchValue, setSearchValue] = useState("pasta");
   const data = useSearch(searchValue);
-  console.log("data", data);
+  console.log(data);
   // console.log("isError", isError);
 
   const onTermSubmit = (term: string) => {
@@ -22,6 +23,9 @@ const SearchScreen = () => {
         onTermSubmit={onTermSubmit}
       />
       <Text>Search Screen</Text>
+      <ResultsList title={"lele"} />
+      <ResultsList title={"lala"} />
+      <ResultsList title={"lolo"} />
     </View>
   );
 };
